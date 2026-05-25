6c3ce912460cb26b4284f3bb61358848a1cb6852
Ayush Choudhary <ka843004@gmail.com>
2026-05-25 21:41:25 +0530

    Counter Project done

--- react-course/src/App.jsx
+++ react-course/src/App.jsx
@@ -52,17 +52,33 @@
 // export default App;
 
 
-import { Welcome, Profile } from './components/profile';
+import { useState } from 'react';
+// import { Welcome, Profile } from './components/profile';
+
+
 
 function App() {
+
+  const [count,setCount] = useState(0);
+
     return (
-        <>
-            <Welcome />
+        <div>
+            {/* <Welcome />
 
             <Profile Name="Ayush" Role="AI Engineer" />
             <Profile Name="Rahul" Role="Backend Developer" />
-            <Profile Name="Sneha" Role="Data Scientist" />
-        </>
+            <Profile Name="Sneha" Role="Data Scientist" /> */}
+
+            <h2>{count}</h2>
+
+            <button onClick={() =>  setCount(count + 1)}> Increase </button>
+            <button onClick={() => { if (count > 0){
+                setCount(count - 1);
+            }
+            }}> Decrease </button>
+            <button onClick={() => setCount(0)}> Reset </button>
+
+        </div>
     );
 }
 
